d8f5781d21b5f4269708ba078c6a918a44a6126a
zakariaeightyahia <zakariaeightyahia@gmail.com>
2024-03-04 15:48:00 +0100

    second commit on the 1st day challenge

---
+++ day_1/day_1_final_programme.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("welcome to the band name generator")
+#2. Ask the user for the city that they grew up in.
+user_name = input("what is the name of the city you grew up in ?")
+#3. Ask the user for the name of a pet.
+pet_name = input("what is the name of your pet ?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your brand name could be -> " + user_name + " " + pet_name)
+#5. Make sure the input cursor shows on a new line:
+print("\n")
---
+++ day_1/day_1_variables.py
@@ -0,0 +1,8 @@
+name = input("enter your name : ")
+print(name)
+#fisrt approach
+print(len(name))
+#second approach 
+length = len(name)
+print(length)
+#print(len(input("enter you name : ")));
